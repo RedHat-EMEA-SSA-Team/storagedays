f115cbb65af8416c34d65a7879872360fde686ea
Tero Ahonen <tahonen@redhat.com>
2018-10-19 14:11:45 +0300

    Added application source code

--- src/main/java/com/redhat/emeatigers/storagedays/EventRepository.java
+++
@@ -1,7 +0,0 @@
-package com.redhat.emeatigers.storagedays;
-
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface EventRepository extends CrudRepository<Event, String> {
-}
