1984da0dd08a4045b467bb70f85e5e527be2c0d6
Tero Ahonen <tahonen@redhat.com>
2018-10-23 17:05:28 +0300

    fixed typo

--- scripts/topology-info.sh
+++ scripts/topology-info.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-
-TOKEN=$(oc get secret heketi-ocs-admin-secret -n $OCS_PROJECT -o yaml | grep 'key:' | awk '{print $2}' | base64 -d)
 OCS_PROJECT=ocs
+TOKEN=$(oc get secret heketi-ocs-admin-secret -n $OCS_PROJECT -o yaml | grep 'key:' | awk '{print $2}' | base64 -d)
 oc rsh -n $OCS_PROJECT dc/heketi-ocs heketi-cli --user admin --secret $TOKEN topology info
